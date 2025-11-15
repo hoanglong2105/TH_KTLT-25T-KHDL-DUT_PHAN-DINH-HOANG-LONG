e60a9a4d6838fea8a3597031f51725eb8631e7e7
hoanglong2105 <147999635+hoanglong2105@users.noreply.github.com>
2025-11-15 17:03:59 +0700

    Update 30.c

--- 30.c
+++ 30.c
@@ -1,103 +1,63 @@
-#include <stdio.h>
-#include <math.h>
-
-int giaiPhuongTrinhBacHai(double a, double b, double c, double *x1, double *x2) {
-    double delta;
-
-    
-    if (a == 0) {
-        if (b == 0) {
-            if (c == 0) {
-                return 0; 
-            } else {
-                return 1; 
-            }
-        } else {
-            #include <stdio.h> 
-#include <math.h> 
-
-double a,b,c,delta;
-
-void input()
+#include<stdio.h>
+#include<math.h>
+int giai_phuong_trinh_bac_hai(double a, double b, double c, double *nghiem1, double *nghiem2);
+int main()
 {
-	scanf("%lf%lf%lf",&a,&b,&c);
-}
-void solve()
-{
-	if (a==0)
+	double a,b,c, nghiem1, nghiem2;
+	scanf("%lf %lf %lf",&a,&b,&c);
+	int so_nghiem=giai_phuong_trinh_bac_hai(a,b,c,&nghiem1,&nghiem2);
+	switch(so_nghiem)
 	{
-		if (b==0)
+		case -1:
+			printf("Phuong trinh co vo so nghiem");
+			break;
+		case 0:
+			printf("Phuong trinh vo nghiem");
+			break;
+		case 1:
+			printf("Phuong trinh co nghiem duy nhat x = %lf",nghiem1);
+			break;
+		case 2:
+			printf("Phuong trinh co 2 nghiem phan biet x1 = %.2lf x2 = %.2lf",nghiem1,nghiem2);
+			break;		
+	}	
+	return 0;
+}
+int giai_phuong_trinh_bac_hai(double a, double b, double c, double *nghiem1, double *nghiem2)
+{
+	if (a == 0)
+	{
+		if (b == 0)
 		{
-			if (c==0)
+			if (c == 0)
 			{
-				printf("Phuong trinh co vo so nghiem");
+				return -1;
 			}
-			else
-			{
-				printf("Phuong trinh vo nghiem");
-			}
+			else return 0;
 		}
 		else
 		{
-			printf("Phuong trinh co nghiem x=%lf",-c/b);
-		}
+			*nghiem1 = -c/b;
+			return 1;
+		} 
 	}
 	else
 	{
-		delta=b*b-4*a*c;
-		if (delta>=0)
+		double delta = b*b - 4*a*c;
+		if (delta<0)
 		{
-			if (delta==0)
-			{
-				printf ("Phuong trinh co nghiem x=%lf",-b/(2*a));
-			}
-			else
-			{
-				printf ("Phuong trinh co 2 nghiem phan biet x1=%lf x2=%lf",(-b+sqrt(delta))/(2*a),(-b-sqrt(delta))/(2*a));
-			}
+			return 0;
+		}
+		else if (delta==0)
+		{
+			*nghiem1=-b/(2*a);
+			return 1;
 		}
 		else
 		{
-			printf ("Phuong trinh vo nghiem");
+			*nghiem1=(-b + sqrt(delta)) / (2*a);
+			*nghiem2=(-b - sqrt(delta)) / (2*a);
+			return 2;
 		}
 	}
-}
-int main() 
-{
-	input();
-	solve();
-	return 0;
-} 
-            *x1 = -c / b;
-            return 4; 
-        }
-    }
-
-   
-    delta = b * b - 4 * a * c;
-
-    if (delta < 0) {
-        return 1; 
-    } else if (delta == 0) {
-       
-        *x1 = *x2 = -b / (2 * a);
-        return 2; 
-    } else {
-        
-        *x1 = (-b + sqrt(delta)) / (2 * a);
-        *x2 = (-b - sqrt(delta)) / (2 * a);
-        return 3; 
-    }
-}
-
-int main() {
-    double a, b, c;
-    double nghiem1, nghiem2; 
-
-    
-    
-    scanf("%lf%lf%lf", &a,&b,&c);
-    
-
-    return 0;
 }
